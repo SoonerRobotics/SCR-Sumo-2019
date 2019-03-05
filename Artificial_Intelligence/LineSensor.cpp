ef8da1be3f9e2de492056d880b4492512b91be4a
Cody Black <codyblack@ou.edu>
2019-03-05 17:31:17 -0600

    Use median to determine threshold value

--- Artificial_Intelligence/LineSensor.cpp
+++ Artificial_Intelligence/LineSensor.cpp
@@ -1,5 +1,6 @@
 #include "LineSensor.h"
 #include <Arduino.h>
+#include <ArduinoSort.h>
 
 LineSensor::LineSensor() {
   
@@ -7,14 +8,23 @@ LineSensor::LineSensor() {
 
 LineSensor::LineSensor(int dataPin) {
   this->dataPin = dataPin;
-  reflectanceThreshold = analogRead(dataPin);
   pinMode(dataPin, INPUT);
+  // Collect sample values of ground reflectance at robot's starting position
+  int values[20];
+  for (int i = 0; i < 20; i++) {
+    values[i] = analogRead(dataPin);
+  }
+  sortArray(values, 20);
+  
+  // Set threshold to median of sample values
+  // TODO: Replace 1 with different value determined by testing line sensor
+  reflectanceThreshold = values[20 / 2] - 1;
 }
 
 LineSensor::LineSensor(int dataPin, int threshold) {
   this->dataPin = dataPin;
-  reflectanceThreshold = threshold;
   pinMode(dataPin, INPUT);
+  reflectanceThreshold = threshold;
 }
 
 bool LineSensor::seeWhite() {
