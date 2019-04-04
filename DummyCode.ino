5e271cd96845a8ad8af05057c546bb8bb8f0c292
medgar591 <matthewaedgar@gmail.com>
2019-04-04 17:38:47 -0500

    Added SearchLoop pseudocode

--- DummyCode.ino
+++ DummyCode.ino
@@ -1,6 +1,6 @@
 //Pseudocode for purposes of working out the loop structure
 var pos; // Some custom class or variable for tracking last known enemy location
-final int COMBAT_DISTANCE;
+final int COMBAT_DISTANCE; //Distance cutoff for engagement logic - Needs to be set
 
 
 void setup() {
@@ -25,5 +25,50 @@ void loop() {
       case side.LEFT:
         zeroDegreeTurn(pos.getSide());
         break;
+  }
+}
+
+pos searchLoop(){
+  pos x;
+  while (true) {
+    pos x;
+    if ( FRONT_SENSOR1.blocked() ){
+      x = new Pos(FRONT_SENSOR1.getInfo(), FRONT_SENSOR2.getInfo());
+      return x;
+    }
+    else if ( FRONT_SENSOR2.blocked() ){
+      x = new Pos(FRONT_SENSOR2.getInfo(), FRONT_SENSOR2.getInfo());
+      return x;
+    }
+    else if ( SIDE_SENSOR1.blocked() ){
+      x = new Pos(SIDE_SENSOR1.getInfo());
+      return x;
+    }
+    else if ( SIDE_SENSOR2.blocked() ){
+      x = new Pos(SIDE_SENSOR2.getInfo());
+      return x;
+    }
+    else if ( BACK_SESNSOR.blocked() ){
+      x = new Pos(BACK_SENSOR.getInfo());
+      return x;
+    }
+  
+    pos lastKnown = pos;
+  
+    switch (lastKnown.getSide()){ //getSide will need to return an enum with each of the 4 sides the enemy bot is on/was on
+        case side.FRONT:
+          if (lastKnown.getF1Dist() >= lastKnown.getF2Dist()) {
+            turnToFaceRight();
+          else
+            turnToFaceLeft();
+          break;
+        case side.BACK:
+          backTurn();
+          break;
+        case side.RIGHT:
+        case side.LEFT:
+          frontTurn(lastKnown.getSide());
+          break;
+    }
   }
 }
